aa5c262f1958ed7520aea35a160cc084fd1c243f
David Ballesteros <david_ballesteros@outlook.com>
2025-08-07 01:34:03 +0800

    stream subscription

--- stream_demo/lib/main.dart
+++ stream_demo/lib/main.dart
@@ -37,6 +37,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   late NumberStream numberStream;
 
   late StreamTransformer transformer;
+  late StreamSubscription subscription;
 
   void changeColor() async {
     colorStream.getColors().listen((eventColor) {
@@ -49,8 +50,19 @@ class _StreamHomePageState extends State<StreamHomePage> {
   void addRandomNumber() {
     Random random = Random();
     int myNum = random.nextInt(10);
-    numberStream.addNumberToSink(myNum);
+    if (!numberStreamController.isClosed) {
+      numberStream.addNumberToSink(myNum);
+    } else {
+      setState(() {
+        lastNumber = -1;
+      });
+    }
+    // numberStream.addNumberToSink(myNum);
     // numberStream.addError();
+  }
+
+  void stopStream() {
+    numberStreamController.close();
   }
 
   @override
@@ -60,6 +72,19 @@ class _StreamHomePageState extends State<StreamHomePage> {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
+    subscription = stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
+    subscription.onError((error) {
+      setState(() {
+        lastNumber = -1;
+      });
+    });
+    subscription.onDone(() {
+      print('OnDone was called');
+    });
     // stream
     //     .listen((event) {
     //       setState(() {
@@ -75,28 +100,28 @@ class _StreamHomePageState extends State<StreamHomePage> {
     colorStream = ColorStream();
     changeColor();
 
-    transformer = StreamTransformer<int, int>.fromHandlers(
-      handleData: (value, sink) {
-        sink.add(value * 10);
-      },
-      handleError: (error, trace, sink) {
-        sink.add(-1);
-      },
-      handleDone: (sink) => sink.close(),
-    );
+    // transformer = StreamTransformer<int, int>.fromHandlers(
+    //   handleData: (value, sink) {
+    //     sink.add(value * 10);
+    //   },
+    //   handleError: (error, trace, sink) {
+    //     sink.add(-1);
+    //   },
+    //   handleDone: (sink) => sink.close(),
+    // );
 
-    stream
-        .transform(transformer)
-        .listen((event) {
-          setState(() {
-            lastNumber = event;
-          });
-        })
-        .onError((error) {
-          setState(() {
-            lastNumber = -1;
-          });
-        });
+    // stream
+    //     .transform(transformer)
+    //     .listen((event) {
+    //       setState(() {
+    //         lastNumber = event;
+    //       });
+    //     })
+    //     .onError((error) {
+    //       setState(() {
+    //         lastNumber = -1;
+    //       });
+    //     });
   }
 
   @override
@@ -121,6 +146,10 @@ class _StreamHomePageState extends State<StreamHomePage> {
               ElevatedButton(
                 onPressed: () => addRandomNumber(),
                 child: Text('New Random Number'),
+              ),
+              ElevatedButton(
+                onPressed: () => stopStream(),
+                child: const Text('Stop subscription'),
               ),
             ],
           ),
